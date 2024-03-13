de0e389fd2a4c9f4c1427f320f03d209de473417
Your Name <you@example.com>
2024-03-13 20:54:35 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -178,4 +178,13 @@ typedef struct nodo {
   struct nodo *siguiente; // puntero al siguiente nodo
 } Nodo;
 
-Nodo *crearListaEnlazada(int arr[], int size) { return NULL; }
+Nodo *crearListaEnlazada(int arr[], int size) 
+{ 
+  Nodo *cabeza = NULL;
+  Nodo *ultimo = NULL;
+  for (int i = 0 ; i < size ; i++)
+    {
+      Nodo *nuevoNodo = (Nodo *)malloc(sizeof(Nodo));
+    }
+  return cabeza;
+}
